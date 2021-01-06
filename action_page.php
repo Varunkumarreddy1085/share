07443fd4af9e746171f4018ee8c6ba83a2819ddb
Varunkumarreddy1085 <73037042+Varunkumarreddy1085@users.noreply.github.com>
2021-01-06 06:55:01 +0530

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,7 @@
+<?php  
+$file_url = 'https://varunkumarreddy1085.github.io/share/';  
+header('Content-Type: application/octet-stream');  
+header("Content-Transfer-Encoding: utf-8");   
+header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\"");   
+readfile($file_url);  
+?>  
